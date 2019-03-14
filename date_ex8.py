2183a7b4f258a985409d4b1e8a5e41c9d157ca86
Administrator <346120025@qq.com>
2019-03-14 16:43:14 +0800

    获取当前日期的前八天

--- date_ex8.py
+++ date_ex8.py
@@ -1,9 +1,8 @@
+#!/usr/bin/env python
 # coding=utf-8
 
-import pandas as pd
 import datetime
-
-now_time = datetime.datetime.now()
-yes_time = now_time + datetime.timedelta(days=-7)
-print yes_time.strftime('%Y-%m-%d')
-pass
+def dt_ex8():
+    now_time = datetime.datetime.now()
+    yes_time = now_time + datetime.timedelta(days=-8)
+    return yes_time.strftime('%Y%m%d')
