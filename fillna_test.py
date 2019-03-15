736b88d126f955d77862fbd0e023ce7d2d23a3a1
Administrator <346120025@qq.com>
2019-03-15 17:00:34 +0800

    测试print的多字段显示

--- fillna_test.py
+++ fillna_test.py
@@ -8,8 +8,9 @@ import numpy as np
 
 data={'is_over':['1','2','3',np.nan,np.nan],
       'this_over_days':[np.nan,'2','3','3',np.nan]}
-
+com='collection_result'
 df=pd.DataFrame(data=data)
 df1=df.fillna('0')
 df2=df.fillna({'is_over':'2','this_over_days':'0'})
+print '文件 %s 的字段分别为%s'%(com,df2.columns)
 pass
