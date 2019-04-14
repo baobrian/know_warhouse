f639fc7143afbb0d2422c68783b7f0337470d2c4
baozilin <bzlin0916@126.com>
2019-04-14 15:17:28 +0800

    更新首次提款率

--- povit_table_cohort_draw.py
+++ povit_table_cohort_draw.py
@@ -9,13 +9,13 @@ import os
 def cohort_register(pk,df):
     regist=pd.DataFrame()
     data=df[pk]
-    data['real_value']=1
-    data['apply_value']=data['apply_time'].map(lambda x: 1 if not pd.isnull(x) else 0)
-    total_reg=data.groupby(['real_time']).size()
+    data['draw_value']=1
+    data['appl_value']=data['appl_dt'].map(lambda x: 1 if not pd.isnull(x) else 0)
+    total_reg=data.groupby(['limit_actv_tm']).size()
     total_reg.name='reg_total'
     regist['total']=total_reg
-    reg_temp=pd.pivot_table(data=data,values='apply_value',\
-               index='real_time',columns='apply_intval',\
+    reg_temp=pd.pivot_table(data=data,values='appl_value',\
+               index='limit_actv_tm',columns='draw_intval',\
                aggfunc=np.sum)
     cohort_reg=reg_temp.reindex(columns=[i for i in range(0,31,1)]).fillna(0)
     cohort_30=regist.join(cohort_reg)
@@ -23,15 +23,16 @@ def cohort_register(pk,df):
 
 
 if __name__ == '__main__':
-    path='/Users/baozilin/Downloads/data/dd/'
-    pk=['real_time','apply_time','apply_intval']
+    path='/Users/baozilin/Downloads/data/cohort_data/data/draw/data/'
+    pk=['limit_actv_tm','appl_dt','draw_intval']
     dirs=os.listdir(path)
     result_cohort=pd.DataFrame()
     for fl in dirs:
         temp=pd.read_csv(path+fl)
         cohort_temp=cohort_register(df=temp,pk=pk)
         result_cohort=result_cohort.append(cohort_temp)
-    result_cohort.to_excel()
+    os.chdir(path)
+    result_cohort.to_excel('draw.xlsx',header=True)
 
 
 
