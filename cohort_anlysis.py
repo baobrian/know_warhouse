1542a3a3e996a1138f0bbe6560d2b1f659d1d07b
baozilin <bzlin0916@126.com>
2019-09-20 22:31:55 +0800

    完善

--- cohort_anlysis.py
+++ cohort_anlysis.py
@@ -1,12 +1,10 @@
 # coding=utf-8
-
 import pandas as pd
 import numpy as np
 import os
 
-
-class CohortAnalysis():
-    def __init__(self,data,stat_time,end_time=None,intrv=None,boundary=30):
+class CohortAnalysis() :
+    def __init__(self, data, stat_time, end_time=None, intrv=None, boundary=30):
         self.data=data
         self.stat_time=stat_time
         self.end_time=end_time
@@ -21,21 +19,20 @@ class CohortAnalysis():
             pk=[self.stat_time,self.intrv]
             df = self.data[pk]
             df['intrv']=df[self.intrv]
-        if self.stat_time is not None and self.end_time is not None and self.intrv is  None:
+        if self.stat_time is not None and self.end_time is not None and self.intrv is None:
             pk=[self.stat_time,self.end_time]
             df=self.data[pk]
             for com in pk:
                 df[com]=pd.to_datetime(df[com],format='%Y/%m/%d')
             df['intrv']=(df[pk[1]]-df[pk[0]]).map(lambda x:x.days)
             df[pk[0]]=df[pk[0]].dt.date
-            # df['_valid_value'] = df[self.column].map(lambda x: 1 if not pd.isnull(x) else 0)
         df['_valid_value'] = df['intrv'].map(lambda x: 1 if not pd.isnull(x) else 0)
         df['count_value'] = 1
         cohort_total = df.groupby([self.stat_time]).size()
         cohort_total.name = 'cohortanalysis'
         result['daily_sum'] = cohort_total
-        temp = pd.pivot_table(data=df, values='_valid_value', \
-                                  index=self.stat_time, columns='intrv', \
+        temp = pd.pivot_table(data=df, values='_valid_value',\
+                                  index=self.stat_time, columns='intrv',\
                                   aggfunc=np.sum)
         _result = temp.reindex(columns=[i for i in range(0, self.boundary, 1)]).fillna(0)
         # _result['Col_sum']=_result.apply(lambda x:x.sum(),axis=1)
@@ -52,8 +49,6 @@ if __name__ == '__main__':
     files = os.listdir(path)
     data=pd.read_csv(path+files[0],index_col=0)
     print data.info()
-    print data.columns
-    print data['user_id'][(data['lst_suc_limit']=='2018/10/21') & (data['last_used_time']=='2018/10/30')]
     cohort=CohortAnalysis(data=data,stat_time='lst_suc_limit',end_time='last_used_time',boundary=180)
     os.chdir(file_save_path)
     cohort.analysis_cohort()
